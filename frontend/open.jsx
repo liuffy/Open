383778138bd0e50ae1f5c26ee7b620e89261a1fb
Sophia Liu <sliu3@wellesley.edu>
2017-02-28 20:00:52 -0800

    More or less have figured out the API

--- frontend/open.jsx
+++ frontend/open.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {getBusinesses, getLocalBusinesses} from './utils/yelp_api_utils';
+import {getBusinesses, getLocalBusinesses, getBusinessHours} from './utils/yelp_api_utils';
 
 document.addEventListener('DOMContentLoaded', () => {
 	window.getBusinesses = getBusinesses;
 	window.getLocalBusinesses = getLocalBusinesses;
+	window.getBusinessHours = getBusinessHours;
   const rootEl = document.getElementById('root');
   ReactDOM.render(<h1>Open is open!</h1>, rootEl);
 });
--- frontend/utils/yelp_api_utils.js
+++ frontend/utils/yelp_api_utils.js
@@ -3,6 +3,7 @@ const yelp = require('../../yelp-fusion/fusion/node/node_modules/yelp-fusion');
 const clientId = 'APBMKv2E9sl-ik2-8fTW6Q';
 const clientSecret = 'LWIK70PM6wSSSRhTPpNNsIBOvwViWG3OuHHpEz1gkMLFd6yPHK6ywDO8mEz4DscH';
 
+// 
 let searchRequest;
 
 function findLocation(){
@@ -23,7 +24,6 @@ function findLocation(){
 }
 
 // This is the function I'm trying to fix 
-
 export const getLocalBusinesses = (nameInput) =>{
 
  findLocation()
@@ -36,12 +36,16 @@ export const getLocalBusinesses = (nameInput) =>{
   };
 
 
-  yelp.accessToken(clientId, clientSecret).then(response => {
+yelp.accessToken(clientId, clientSecret).then(response => {
     const client = yelp.client(response.jsonBody.access_token);
 
     client.search(searchRequest).then(response => {
-      const prettyJson = JSON.stringify(response.jsonBody.businesses, null, 4);
-      console.log(prettyJson);
+      // const prettyJson = JSON.stringify(response.jsonBody.businesses, null, 4);
+
+      response.jsonBody.businesses.forEach(function(business){
+        businessIds.push(business.id)
+      })
+      console.log(businessIds);
     });
   }).catch(e => {
     console.log(e);
@@ -52,21 +56,62 @@ export const getLocalBusinesses = (nameInput) =>{
 
 // This works fine
 export const getBusinesses = (nameInput, locationInput) => {
-  const searchRequest = {
+  searchRequest = {
     term: nameInput,
     location: locationInput,
-    limit: '2'
+    limit: '3'
   };
+
+  var businessIds = [];
 
   yelp.accessToken(clientId, clientSecret).then(response => {
     const client = yelp.client(response.jsonBody.access_token);
 
     client.search(searchRequest).then(response => {
-      const prettyJson = JSON.stringify(response.jsonBody.businesses, null, 4);
-      console.log(prettyJson);
+      response.jsonBody.businesses.forEach(function(business){
+        businessIds.push(business.id)
+      })
+      console.log(businessIds);
     });
   }).catch(e => {
     console.log(e);
   });
 
+}
+
+
+export const getBusinessHours = (iDInput) =>{
+
+  var businessHours = {};
+
+  yelp.accessToken(clientId, clientSecret).then(response => {
+    const client = yelp.client(response.jsonBody.access_token);
+
+    client.business(iDInput).then(response => {
+      
+      response.jsonBody.hours[0].open.forEach(function(dayObject){
+          businessHours[dayObject.day] = [dayObject.start, dayObject.end]
+      })
+
+      // Let's push all of these into an object? 
+      // Hours open
+      console.log(businessHours)
+      // Formatted address
+      console.log(response.jsonBody.location.display_address.toString())
+      // Phone number
+      console.log(response.jsonBody.phone)
+      // Boolean open value
+
+      var openOrNot;
+      if (response.jsonBody.hours[0].is_open_now){
+        openOrNot = "open"
+      } else {
+        openOrNot = "closed"
+      }
+      console.log(openOrNot)
+    });
+    
+  }).catch(e => {
+    console.log(e);
+  });
 }
