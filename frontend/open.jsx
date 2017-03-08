b4f298d63501af4cb6a7ed200d847f90530fb9ee
Sophia Liu <sliu3@wellesley.edu>
2017-03-08 10:52:07 -0800

    Cleaned up some filse

--- frontend/open.jsx
+++ frontend/open.jsx
@@ -2,19 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from './store/store';
 import Root from './components/root';
-import {createLocalResults, createCityResults} from './actions/search_actions';
 
-import {getBusinessesByCity, getLocalBusinesses, getBusinessData} from './utils/yelp_api_utils';
 
 document.addEventListener('DOMContentLoaded', () => {
-	// window.getBusinessesByCity = getBusinessesByCity;
-	// window.getLocalBusinesses = getLocalBusinesses;
-	window.getBusinessData = getBusinessData;
 	// store.dispatch(action()) to see if action creators work;
 	let store = configureStore();
 	window.store = store;
-	window.createLocalResults = createLocalResults;
-	window.createCityResults = createCityResults;
   const root = document.getElementById('root');
   ReactDOM.render(<Root store={store}/>, root);
 });
